0802a2e8706e07ac523da8a79c1656196e874eb6
Velasco <quarantine.sleep480@passinbox.com>
2024-09-27 14:01:18 +0200

    aa

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -5,19 +5,26 @@ import java.util.List;
 
 public class Exercise20 {
 
+    static List<Person> peopleList = new ArrayList<>();
     public static List<Person> getPeopleList() {
+        if (peopleList.isEmpty()) {
+            peopleList.add(new Person("Juan", "Juan"));
+            peopleList.add(new Teacher("Juan", "Juan", "Matematicas"));
+            peopleList.add(new PoliceOfficer("Juan", "Juan", "Sargento"));
+            peopleList.add(new Doctor("Juan", "Juan", "Cirujano"));
+        }
 
+        return peopleList;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person person : stringList) {
+            person.getDetails();
+        }
     }
 
     public static void main(String[] args) {
         List<Person> stringList = getPeopleList();
         showPeopleDetails(stringList);
     }
-
-
-
 }
